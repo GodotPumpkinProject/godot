c099a9ff245389682403aa24e57b717eb2a14856
GrahamMueller(Desktop) <>
2023-09-21 18:58:16 -0700

    Added basic build and cleanup scripts.

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+set -e
+
+
+scons --clean 2>&1 | tee clean.sh.log
+find . -name \*.gen.* -type f -delete 2>&1 | tee -a clean.sh.log
+find . -name \*.lib -type f -delete 2>&1 | tee -a clean.sh.log
+find . -name \*.obj -type f -delete 2>&1 | tee -a clean.sh.log
+find . -name \*.lnk -type f -delete 2>&1 | tee -a clean.sh.log
+echo "Complete"
+
